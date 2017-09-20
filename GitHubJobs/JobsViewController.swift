1fa49884ddac67457aaab863dc69ea3411f0aba9
Phil <phil@pccole.com>
2017-09-20 18:03:49 -0400

    Moved Tableview to JobsViewController

--- GitHubJobs/JobsViewController.swift
+++ GitHubJobs/JobsViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class JobsViewController: UIViewController {
 
+	lazy var spinner: UIActivityIndicatorView = {
+		let view = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
+		view.translatesAutoresizingMaskIntoConstraints = false
+		return view
+	}()
+	
+	lazy var spinnerConstraints: [NSLayoutConstraint] = {
+		return [
+			self.view.centerXAnchor.constraint(equalTo: self.spinner.centerXAnchor),
+			self.view.centerYAnchor.constraint(equalTo: self.spinner.centerYAnchor)
+		]
+	}()
+	
+	lazy var tableViewConstraints: [NSLayoutConstraint] = {
+		return [
+			self.tableview.topAnchor.constraint(equalTo: self.tableview.topAnchor),
+			self.tableview.leadingAnchor.constraint(equalTo: self.tableview.leadingAnchor),
+			self.tableview.trailingAnchor.constraint(equalTo: self.tableview.trailingAnchor),
+			self.tableview.bottomAnchor.constraint(equalTo: self.tableview.bottomAnchor)
+		]
+	}()
+	
+	let tableview: UITableView = {
+		let t = UITableView()
+		t.translatesAutoresizingMaskIntoConstraints = false
+		return t
+	}()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		title = "Jobs"
+		view.addSubview(tableview)
+		view.sendSubview(toBack: tableview)
+		NSLayoutConstraint.activate(tableViewConstraints)
+		view.addSubview(spinner)
+		NSLayoutConstraint.activate(spinnerConstraints)
     }
 
     override func didReceiveMemoryWarning() {
--- GitHubJobs/SearchViewController.swift
+++ GitHubJobs/SearchViewController.swift
@@ -20,53 +20,21 @@ class SearchViewController: UIViewController {
 		return button
 	}()
 	
-	lazy var spinner: UIActivityIndicatorView = {
-		let view = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
-		view.translatesAutoresizingMaskIntoConstraints = false
-		return view
-	}()
-	
-	lazy var spinnerConstraints: [NSLayoutConstraint] = {
-		return [
-			self.view.centerXAnchor.constraint(equalTo: self.spinner.centerXAnchor),
-			self.view.centerYAnchor.constraint(equalTo: self.spinner.centerYAnchor)
-		]
-	}()
-	
-	lazy var tableViewConstraints: [NSLayoutConstraint] = {
-		return [
-			self.tableview.topAnchor.constraint(equalTo: self.tableview.topAnchor),
-			self.tableview.leadingAnchor.constraint(equalTo: self.tableview.leadingAnchor),
-			self.tableview.trailingAnchor.constraint(equalTo: self.tableview.trailingAnchor),
-			self.tableview.bottomAnchor.constraint(equalTo: self.tableview.bottomAnchor)
-		]
-	}()
-	
-	let tableview: UITableView = {
-		let t = UITableView()
-		t.translatesAutoresizingMaskIntoConstraints = false
-		return t
-	}()
+
 	
     override func viewDidLoad() {
         super.viewDidLoad()
-		title = "GitHub Jobs"
+		title = "Search Jobs"
 		view.backgroundColor = UIColor.lightGray
-		view.addSubview(tableview)
-		view.sendSubview(toBack: tableview)
-		NSLayoutConstraint.activate(tableViewConstraints)
-		view.addSubview(spinner)
-		NSLayoutConstraint.activate(spinnerConstraints)
 		navigationController?.navigationItem.rightBarButtonItem = searchButton
     }
 	
 	@objc private func searchTapped() {
-		view.bringSubview(toFront: tableview)
-		view.bringSubview(toFront: spinner)
+		
 	}
 	
 	@objc private func clearTapped() {
-		view.sendSubview(toBack: tableview)
+		
 	}
 	
 }
