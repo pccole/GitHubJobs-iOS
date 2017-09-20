d9deb9168bb49385df48c19081073fcae75e9d37
Phil <phil@pccole.com>
2017-09-19 21:38:02 -0400

    Added Jobs view controller to display the results of the jobs

---
+++ GitHubJobs/JobsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  JobsViewController.swift
+//  GitHubJobs
+//
+//  Created by Phil Cole on 9/19/17.
+//  Copyright © 2017 Phil Cole. All rights reserved.
+//
+
+import UIKit
+
+class JobsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
