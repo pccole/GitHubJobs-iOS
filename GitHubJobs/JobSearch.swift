f8109a17a7248f2ea48947a0f2aa8b3948397510
Phil <phil@pccole.com>
2017-09-24 19:18:11 -0400

    Added Parameters computed property to JobSearch

--- GitHubJobs/JobSearch.swift
+++ GitHubJobs/JobSearch.swift
@@ -14,4 +14,22 @@ struct JobSearch {
 	let latitude:Double?
 	let longitude:Double?
 	let fullTime = true
+	
+	var parameters: [String: Any]? {
+		if let location = location {
+			let parameters:[String:Any] = ["location":location,
+			                  "description":description,
+			                  "full_time":fullTime]
+			return parameters
+		} else if let latitude = latitude, let longitude = longitude {
+			let parameters:[String:Any] = ["latitude":latitude,
+			                               "longitude":longitude,
+			                               "description":description,
+			                               "full_time":fullTime]
+			return parameters
+		} else {
+			return nil
+		}
+	}
+	
 }
