24bb9644020a27a28006f92271a1450c7e2d4175
Phil <phil@pccole.com>
2017-10-01 01:59:08 -0400

    Setup populating the Jobs View Controller table view

--- GitHubJobs/JobsViewController.swift
+++ GitHubJobs/JobsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class JobsViewController: UIViewController {
 
+	private var jobs:[Job]
+	
 	private lazy var spinner: UIActivityIndicatorView = {
 		let view = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.whiteLarge)
 		view.translatesAutoresizingMaskIntoConstraints = false
@@ -23,28 +25,35 @@ class JobsViewController: UIViewController {
 		]
 	}()
 	
-	private lazy var tableViewConstraints: [NSLayoutConstraint] = {
-		return [
-			self.tableview.topAnchor.constraint(equalTo: self.tableview.topAnchor),
-			self.tableview.leadingAnchor.constraint(equalTo: self.tableview.leadingAnchor),
-			self.tableview.trailingAnchor.constraint(equalTo: self.tableview.trailingAnchor),
-			self.tableview.bottomAnchor.constraint(equalTo: self.tableview.bottomAnchor)
-		]
-	}()
-	
 	private lazy var tableview: UITableView = {
 		let t = UITableView()
 		t.translatesAutoresizingMaskIntoConstraints = false
 		t.register(JobTableViewCell.self, forCellReuseIdentifier: JobTableViewCell.description())
+		t.delegate = self
+		t.dataSource = self
 		return t
 	}()
+	
+	init(jobs:[Job]) {
+		self.jobs = jobs
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		self.jobs = []
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
 	
     override func viewDidLoad() {
         super.viewDidLoad()
 		title = "Jobs"
 		view.addSubview(tableview)
 		view.sendSubview(toBack: tableview)
-		NSLayoutConstraint.activate(tableViewConstraints)
+		tableview.pinToSuperview()
 		view.addSubview(spinner)
 		NSLayoutConstraint.activate(spinnerConstraints)
     }
@@ -53,4 +62,22 @@ class JobsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension JobsViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		//
+	}
+}
+
+extension JobsViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return jobs.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: JobTableViewCell.description(), for: indexPath) as! JobTableViewCell
+		cell.job = jobs[indexPath.row]
+		return cell
+	}
 }
