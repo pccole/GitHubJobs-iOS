c98608ea9ce2b5fcbd68c00e12b993b50bef15a9
Phil <phil@pccole.com>
2017-10-01 01:15:48 -0400

    Added jobs table view cell

---
+++ GitHubJobs/Cells/JobTableViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  JobTableViewCell.swift
+//  GitHubJobs
+//
+//  Created by Phil Cole on 10/1/17.
+//  Copyright © 2017 Phil Cole. All rights reserved.
+//
+
+import UIKit
+
+class JobTableViewCell: UITableViewCell {
+
+	var job:Job! {
+		didSet {
+			guard let job = job else { return }
+			populateInformation(with: job)
+		}
+	}
+	
+	
+	private lazy var stackView: UIStackView = {
+		let view = UIStackView(arrangedSubviews: [titleLabel, locationLabel, fullTimeLabel, postedLabel])
+		view.translatesAutoresizingMaskIntoConstraints = false
+		view.axis = .vertical
+		view.spacing = 5
+		return view
+	}()
+	
+	private lazy var titleLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	private lazy var locationLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	private lazy var fullTimeLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	private lazy var postedLabel: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		return label
+	}()
+	
+	private lazy var companyLogoImageView: UIImageView = {
+		let imageView = UIImageView()
+		return imageView
+	}()
+	
+	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		accessoryView = companyLogoImageView
+		contentView.addSubview(stackView)
+		stackView.pinToSuperview()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+	
+	private func populateInformation(with job:Job) {
+		
+	}
+
+}
