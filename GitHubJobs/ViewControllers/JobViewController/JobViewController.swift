5dc4a0640ad4d4a2dfe19cf7d75c329a6834c10a
Phil <phil@pccole.com>
2017-10-01 19:16:36 -0400

    Added initializers to JobViewController

--- GitHubJobs/ViewControllers/JobViewController/JobViewController.swift
+++ GitHubJobs/ViewControllers/JobViewController/JobViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class JobViewController: UIViewController {
 
+	private let job:Job
+	
+	init(job:Job) {
+		self.job = job
+		super.init(nibName: nil, bundle: nil)
+	}
+	
+	private override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		job = Job()
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
     override func viewDidLoad() {
         super.viewDidLoad()
     }
