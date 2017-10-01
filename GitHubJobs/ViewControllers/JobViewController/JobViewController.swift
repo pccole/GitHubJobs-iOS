67561fa86fecfb016b9861751ef1b0dedfe11ac0
Phil <phil@pccole.com>
2017-10-01 18:54:27 -0400

    Added JobViewController

---
+++ GitHubJobs/ViewControllers/JobViewController/JobViewController.swift
@@ -0,0 +1,18 @@
+//
+//  JobViewController.swift
+//  GitHubJobs
+//
+//  Created by Phil Cole on 10/1/17.
+//  Copyright © 2017 Phil Cole. All rights reserved.
+//
+
+import UIKit
+
+class JobViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+	
+}
