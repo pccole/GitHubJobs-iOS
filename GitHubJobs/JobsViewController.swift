231598474d20e7a926a006617e45a08c96a0d1c2
Phil <phil@pccole.com>
2017-10-01 19:05:19 -0400

    changed language to description in jobsviewcontroller

--- GitHubJobs/JobsViewController.swift
+++ GitHubJobs/JobsViewController.swift
@@ -66,7 +66,7 @@ class JobsViewController: UIViewController {
 
 extension JobsViewController: UITableViewDelegate {
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		//
+		
 	}
 }
 
